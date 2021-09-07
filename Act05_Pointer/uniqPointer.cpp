cad4918be68645b9881ee38870a6f760d592298a
Attaphon <plianattaphon@gmail.com>
2021-09-07 11:38:13 +0700

    Update code (input space)

--- Act05_Pointer/uniqPointer.cpp
+++ Act05_Pointer/uniqPointer.cpp
@@ -1,10 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main() {
-	char str[99];
-	char* Strptr = str;
+	char str[99], *Strptr;
+	Strptr = str;
 	printf("Input String : ");
-	scanf("%s", str);
+	scanf("%[^\n]s", str);
 	printf("Output String : ");
 	while (*Strptr != '\0') {
 		if (*Strptr != *(Strptr + 1)) {
